a545ba6a7cfc38b5f7fe569264c296ddb27fe454
Eduardo Nicolas Fernandez <eduardonicolasfernandez1234@gmail.com>
2020-10-26 00:00:55 -0400

    feat: calculadora

--- src/app/Pages/calculadora/calculadora.page.ts
+++ src/app/Pages/calculadora/calculadora.page.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculadoraPage implements OnInit {
 
+  valor1: number;
+  valor2: number;
+  res: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calcular(){
+    if(this.valor1 > 0 && this.valor2 > 0){
+      this.res = this.valor1 * this.valor2; 
+    }else{
+      console.log("No se puede calcular");
+    }
   }
 
 }
